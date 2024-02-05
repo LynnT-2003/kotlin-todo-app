c8c617dc8532c84859aee0a3b8b9c1302c2b840b
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-02-06 04:07:44 +0700

    Feat: Completed UI Navigation and Signup/Login Authentication

---
+++ app/src/main/java/com/example/todo/fragments/SignInFragment.kt
@@ -0,0 +1,66 @@
+package com.example.todo.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.todo.R
+import com.example.todo.databinding.FragmentSignInBinding
+import com.example.todo.databinding.FragmentSignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var navControl: NavController
+    private lateinit var binding: FragmentSignInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        registerEvents()
+    }
+
+    private fun init(view: View){
+        navControl = Navigation.findNavController(view)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun registerEvents(){
+
+        binding.textViewSignUp.setOnClickListener{
+            navControl.navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            val email = binding.emailEt.text.toString().trim()
+            val pass = binding.passEt.text.toString().trim()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+                    auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(
+                        OnCompleteListener {
+                            if (it.isSuccessful) {
+                                Toast.makeText(context, "Logged In Successfully.", Toast.LENGTH_SHORT).show()
+                                navControl.navigate(R.id.action_signInFragment_to_homeFragment)
+                            } else {
+                                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+                            }
+                        })
+            }
+        }
+    }
+}
